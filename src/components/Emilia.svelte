fb9125109009988eb4d5296737931de04f453496
Artem <artur8259@outlook.com>
2023-05-01 14:36:11 +0200

    I love Emilia

--- src/components/Emilia.svelte
+++ src/components/Emilia.svelte
@@ -1,11 +1,23 @@
 <script lang="ts">
   import * as PIXI from "pixi.js";
-  import { Live2DModel } from "pixi-live2d-display";
+  import { Live2DModel, MotionPriority } from "pixi-live2d-display";
   import { onDestroy, onMount } from "svelte";
 
   let canvas: HTMLCanvasElement;
   let app: PIXI.Application;
   let model: Live2DModel;
+
+  function getRandomModelUrl() {
+    const baseUrl = "https://cdn.arti3.dev/live2d/";
+    const models = [
+      "emilia01/ac_base_emilia01.model3.json",
+      "emilia_nemaki01/ac_base_emilia_nemaki01.model3.json",
+      "emilia_dress01/ac_base_emilia_dress01.model3.json",
+    ];
+
+    const choice = models[Math.floor(Math.random() * models.length)];
+    return baseUrl + choice;
+  }
 
   onMount(async () => {
     (window as any).PIXI = PIXI;
@@ -17,17 +29,17 @@
       height: 900,
     });
 
-    model = await Live2DModel.from("https://cdn.arti3.dev/live2d/ram/ram.model3.json");
+    model = await Live2DModel.from(getRandomModelUrl());
     app.stage.addChild(model as unknown as PIXI.DisplayObject);
 
     canvas.style.cursor = "pointer";
 
     model.internalModel.motionManager.state.shouldRequestIdleMotion = () => false;
     model.internalModel.motionManager.on("motionFinish", () => {
-      setTimeout(() => model.motion("Idle"), 2000);
+      setTimeout(() => model.motion("Idle", undefined, MotionPriority.IDLE), 2000);
     });
 
-    model.motion("Idle");
+    model.motion("Idle", undefined, MotionPriority.IDLE);
 
     model.scale.set(0.45);
     model.x = -600;
