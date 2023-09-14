1bb59a1bbd0a79829d615ce9c21c490c117bcff0
artie <artur8259@outlook.com>
2023-09-14 16:45:47 +0200

    update astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,6 @@ import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
-import compress from "astro-compress";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), compress({ img: false })],
+  integrations: [tailwind(), svelte()],
 });
