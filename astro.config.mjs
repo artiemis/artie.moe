173285b4d1e6d1c97a2baca651079aad501a4e8d
artie <artiehime@gmail.com>
2024-01-11 17:28:46 +0100

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import svelte from "@astrojs/svelte";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()],
+  integrations: [tailwind(), svelte(), icon()],
+  compressHTML: false,
 });
